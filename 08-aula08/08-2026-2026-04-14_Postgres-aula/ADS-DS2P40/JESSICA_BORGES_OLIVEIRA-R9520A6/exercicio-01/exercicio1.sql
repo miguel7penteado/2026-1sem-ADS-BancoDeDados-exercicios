87f9f21a66c89df01fbbcc338617d0854e14f39f
dataleve <dataleve@gmail.com>
2026-04-14 21:49:22 -0300

    Update exercicio1.sql

--- 08-aula08/08-2026-2026-04-14_Postgres-aula/ADS-DS2P40/JESSICA_BORGES_OLIVEIRA-R9520A6/exercicio-01/exercicio1.sql
+++ 08-aula08/08-2026-2026-04-14_Postgres-aula/ADS-DS2P40/JESSICA_BORGES_OLIVEIRA-R9520A6/exercicio-01/exercicio1.sql
@@ -1,1 +1,10 @@
-//exercicio1
+SELECT 
+	cnpj, status, nome, 
+	data_atividade, estado
+INTO
+    yeda_gomes_dos_santos_custodio_r872696.relatorio01
+FROM 
+	public.cnpj_brasil
+where 
+	estado = 'AC' AND
+	cast(data_atividade as DATE) > '2024-01-01' limit 2000;
